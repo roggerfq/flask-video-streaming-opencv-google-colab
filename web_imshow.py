a57fa646c2382a3937ecf2d6850d283d0bd68844
roggerfq <36053387+roggerfq@users.noreply.github.com>
2021-04-18 08:25:45 -0500

    Solution to "Address already in use"

--- web_imshow.py
+++ web_imshow.py
@@ -1,15 +1,50 @@
-from multiprocessing import Process
 from app import app
+from multiprocessing import Process
 import cv2
-
 from multiprocessing.connection import Client
-
 import time
+
+
 def f(app):
     app.run()
 
 class WebWindow():
   def __init__(self):
+
+    #__________Solution to "Address already in use"_________
+
+      import os
+      import subprocess
+      from subprocess import check_output
+      import re
+      import signal
+
+      port = 5000 #port flask
+
+      def get_pid(name):
+          return check_output(["pidof",name])
+
+      try:
+          data = subprocess.check_output(['lsof', '-i:{}'.format(port)]).decode().split('\n')[1]
+          pid = re.match('^([a-zA-Z0-9]+)(\s+)([0-9]+)\s', data).groups()[2]
+          print('flask: '+ str(pid))
+          os.kill(int(pid), signal.SIGTERM) #or signal.SIGKILL 
+      except:
+          print('pass flask')
+          pass
+
+      try:
+          str_pid_list = get_pid('ngrok').decode("utf-8").split(' ')
+          for str_pid in str_pid_list:
+              pid = int(str_pid)
+              print('ngrok: '+ str(pid))
+              os.kill(pid, signal.SIGTERM) #or signal.SIGKILL 
+      except: 
+          print('pass ngrok')
+          pass
+
+      #________________________________________________________
+
 
       p = Process(target=f, args=(app,))
       p.start()
