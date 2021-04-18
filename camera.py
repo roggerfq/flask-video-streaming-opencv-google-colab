f870187a9e3ec40230e55112373cb95716fff88c
roggerfq <36053387+roggerfq@users.noreply.github.com>
2021-04-18 05:38:34 -0500

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,14 +1,28 @@
 import time
 from base_camera import BaseCamera
+import cv2
+from multiprocessing.connection import Listener
+import sys
 
 
 class Camera(BaseCamera):
-    """An emulated camera implementation that streams a repeated sequence of
-    files 1.jpg, 2.jpg and 3.jpg at a rate of one frame per second."""
-    imgs = [open(f + '.jpg', 'rb').read() for f in ['1', '2', '3']]
-
     @staticmethod
     def frames():
+
+        address = ('localhost', 6000)     # family is deduced to be 'AF_INET'
+        str_key = '1234'
+        listener = Listener(address, authkey=bytes(str_key, encoding= 'utf-8'))
+        conn = listener.accept()
+ 
+
         while True:
-            time.sleep(1)
-            yield Camera.imgs[int(time.time()) % 3]
+           # read current frame
+           data = conn.recv()
+           img = data[0]
+           flag = data[1]
+           if(flag == False):
+              print("close process")
+              sys.exit()
+
+           # encode as a jpeg image and return it
+           yield cv2.imencode('.jpg', img)[1].tobytes()
