cdda133ac9b8901313b54fefd83f760c37c4a904
roggerfq <36053387+roggerfq@users.noreply.github.com>
2021-04-18 05:52:34 -0500

    web_imshow
    A alternative to the function imshow of opencv in Google Colab

---
+++ web_imshow.py
@@ -0,0 +1,36 @@
+from multiprocessing import Process
+from app import app
+import cv2
+
+from multiprocessing.connection import Client
+
+import time
+def f(app):
+    app.run()
+
+class WebWindow():
+  def __init__(self):
+
+      p = Process(target=f, args=(app,))
+      p.start()
+      #p.join()
+
+      address = ('localhost', 6000)
+      str_key = '1234'
+      self.conn = None
+      connected = False
+      while not connected:
+          try:
+             self.conn = Client(address, authkey=bytes(str_key, encoding= 'utf-8'))
+             connected = True
+          except Exception as e:
+             print("wait for socket two seconds")
+             time.sleep(2)
+
+  def imshow(self, img):
+      flag = True
+      self.conn.send([img, flag])
+
+  def close(self):
+      flag = False
+      self.conn.send([None, flag])
